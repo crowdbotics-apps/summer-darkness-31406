36aa7f4514cbfa7bdb2aa7a1debcfe2dda7ee44f
Crowdbotics <team@crowdbotics.com>
2021-10-14 03:06:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "summer_darkness_31406",
+  url: "https://summer_darkness_31406.botics.co",
+  api: "https://summer_darkness_31406.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
